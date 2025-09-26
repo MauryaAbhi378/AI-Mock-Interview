e1f49aeb34c2735f37b2543db2063a92ffbb83ab
MauryaAbhi378 <abhim0505@gmail.com>
2025-09-26 22:45:32 +0530

    Change vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-  },
-})
+    alias: [
+      { find: "src", replacement: path.resolve(__dirname, "src") }
+    ]
+  }
+});
